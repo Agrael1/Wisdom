274cda823fca7f856222e71759eada456a84bc23
Restyled.io <commits@restyled.io>
2023-08-19 22:19:27 +0000

    Restyled by astyle

--- examples/hello-triangle-winrt/app.cpp
+++ examples/hello-triangle-winrt/app.cpp
@@ -75,11 +75,11 @@ void Test::App::Initialize(IUnknown *core_window, uint32_t xwidth, uint32_t xhei
 
     std::array cas2{
         wis::ColorAttachment{
-                .format = wis::SwapchainOptions::default_format,
-                .load = wis::PassLoadOperation::clear },
+            .format = wis::SwapchainOptions::default_format,
+            .load = wis::PassLoadOperation::clear },
         wis::ColorAttachment{
-                .format = wis::SwapchainOptions::default_format,
-                .load = wis::PassLoadOperation::clear }
+            .format = wis::SwapchainOptions::default_format,
+            .load = wis::PassLoadOperation::clear }
     };
 
     render_pass = device.CreateRenderPass({ width, height }, { cas2.data(), swap.StereoSupported() + 1u });
@@ -146,12 +146,12 @@ void Test::App::Frame()
     auto back = swap.GetBackBuffer();
 
     context.TextureBarrier({
-                                   .state_before = wis::TextureState::Present,
-                                   .state_after = wis::TextureState::RenderTarget,
-                                   .access_before = wis::ResourceAccess::Common,
-                                   .access_after = wis::ResourceAccess::RenderTarget,
-                           },
-                           back);
+        .state_before = wis::TextureState::Present,
+        .state_after = wis::TextureState::RenderTarget,
+        .access_before = wis::ResourceAccess::Common,
+        .access_after = wis::ResourceAccess::RenderTarget,
+    },
+    back);
 
     constexpr wis::ColorClear color{ 0.0f, 0.2f, 0.4f, 1.0f };
     constexpr wis::ColorClear color2{ 1.0f, 0.2f, 0.4f, 1.0f };
@@ -171,12 +171,12 @@ void Test::App::Frame()
     context.EndRenderPass();
 
     context.TextureBarrier({
-                                   .state_before = wis::TextureState::RenderTarget,
-                                   .state_after = wis::TextureState::Present,
-                                   .access_before = wis::ResourceAccess::RenderTarget,
-                                   .access_after = wis::ResourceAccess::Common,
-                           },
-                           back);
+        .state_before = wis::TextureState::RenderTarget,
+        .state_after = wis::TextureState::Present,
+        .access_before = wis::ResourceAccess::RenderTarget,
+        .access_after = wis::ResourceAccess::Common,
+    },
+    back);
     context.Close();
     queue.ExecuteCommandList(context);
 
--- wisdom/include/wisdom/vulkan/vk_views.h
+++ wisdom/include/wisdom/vulkan/vk_views.h
@@ -6,26 +6,26 @@
 
 WIS_EXPORT namespace wis
 {
-    using VKAdapterView = std::tuple<vk::PhysicalDevice>;
-    using VKFactoryHandle = std::tuple<wis::shared_handle<vk::Instance>>;
+using VKAdapterView = std::tuple<vk::PhysicalDevice>;
+using VKFactoryHandle = std::tuple<wis::shared_handle<vk::Instance>>;
 
-    using VKDeviceView = wis::shared_handle<vk::Device>;
-    using VKBufferView = vk::Buffer;
-    struct VKTextureView {
-        vk::Image image;
-        vk::Format format;
-    };
-    using VKFenceView = vk::Semaphore;
-    using VKCommandQueueView = vk::Queue;
-    using VKCommandListView = vk::CommandBuffer;
-    using VKPipelineStateView = vk::Pipeline;
-    using VKRootSignatureView = vk::PipelineLayout;
-    struct VKRenderPassView {
-        vk::RenderPass pass;
-        vk::Framebuffer frame;
-        Size2D frame_size;
-    };
+using VKDeviceView = wis::shared_handle<vk::Device>;
+using VKBufferView = vk::Buffer;
+struct VKTextureView {
+    vk::Image image;
+    vk::Format format;
+};
+using VKFenceView = vk::Semaphore;
+using VKCommandQueueView = vk::Queue;
+using VKCommandListView = vk::CommandBuffer;
+using VKPipelineStateView = vk::Pipeline;
+using VKRootSignatureView = vk::PipelineLayout;
+struct VKRenderPassView {
+    vk::RenderPass pass;
+    vk::Framebuffer frame;
+    Size2D frame_size;
+};
 
-    using VKDescriptorSetView = vk::DescriptorSet;
-    using VKDescriptorSetBindView = VKDescriptorSetView;
+using VKDescriptorSetView = vk::DescriptorSet;
+using VKDescriptorSetBindView = VKDescriptorSetView;
 }
