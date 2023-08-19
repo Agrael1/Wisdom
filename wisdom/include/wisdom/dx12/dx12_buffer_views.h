e6359f9783143e7d2e26257586865d8afa9d14e6
Restyled.io <commits@restyled.io>
2023-08-19 13:52:11 +0000

    Restyled by astyle

--- wisdom/include/wisdom/dx12/dx12_buffer_views.h
+++ wisdom/include/wisdom/dx12/dx12_buffer_views.h
@@ -6,38 +6,38 @@
 
 WIS_EXPORT namespace wis
 {
-    class DX12VertexBufferView;
+class DX12VertexBufferView;
 
-    template<>
-    class Internal<DX12VertexBufferView>
+template<>
+class Internal<DX12VertexBufferView>
+{
+public:
+    Internal() = default;
+    Internal(D3D12_VERTEX_BUFFER_VIEW view)
+        : view(view) { }
+
+    [[nodiscard]] D3D12_VERTEX_BUFFER_VIEW GetView() const noexcept
     {
-    public:
-        Internal() = default;
-        Internal(D3D12_VERTEX_BUFFER_VIEW view)
-            : view(view) { }
+        return view;
+    }
 
-        [[nodiscard]] D3D12_VERTEX_BUFFER_VIEW GetView() const noexcept
-        {
-            return view;
-        }
+protected:
+    D3D12_VERTEX_BUFFER_VIEW view;
+};
 
-    protected:
-        D3D12_VERTEX_BUFFER_VIEW view;
-    };
+/// @brief Vertex buffer view
+class DX12VertexBufferView : public QueryInternal<DX12VertexBufferView>
+{
+public:
+    DX12VertexBufferView() = default;
+    explicit DX12VertexBufferView(D3D12_VERTEX_BUFFER_VIEW xhandle)
+        : QueryInternal(xhandle) { }
 
-    /// @brief Vertex buffer view
-    class DX12VertexBufferView : public QueryInternal<DX12VertexBufferView>
+    /// @brief Get the view
+    /// @note This function is only available in the internal API
+    operator D3D12_VERTEX_BUFFER_VIEW() const noexcept
     {
-    public:
-        DX12VertexBufferView() = default;
-        explicit DX12VertexBufferView(D3D12_VERTEX_BUFFER_VIEW xhandle)
-            : QueryInternal(xhandle) { }
-
-        /// @brief Get the view
-        /// @note This function is only available in the internal API
-        operator D3D12_VERTEX_BUFFER_VIEW() const noexcept
-        {
-            return view;
-        }
-    };
+        return view;
+    }
+};
 }
--- wisdom/include/wisdom/vulkan/vk_allocator.h
+++ wisdom/include/wisdom/vulkan/vk_allocator.h
@@ -11,133 +11,133 @@
 
 WIS_EXPORT namespace wis
 {
-    class VKResourceAllocator;
+class VKResourceAllocator;
 
-    template<>
-    class Internal<VKResourceAllocator>
+template<>
+class Internal<VKResourceAllocator>
+{
+public:
+    [[nodiscard]] VmaAllocator GetAllocator() const noexcept
     {
-    public:
-        [[nodiscard]] VmaAllocator GetAllocator() const noexcept
-        {
-            return allocator.get();
-        }
+        return allocator.get();
+    }
 
-    protected:
-        wis::shared_handle<VmaAllocator> allocator;
-    };
+protected:
+    wis::shared_handle<VmaAllocator> allocator;
+};
 
-    /// @brief Resource allocator for Vulkan
-    class VKResourceAllocator : public QueryInternal<VKResourceAllocator>
+/// @brief Resource allocator for Vulkan
+class VKResourceAllocator : public QueryInternal<VKResourceAllocator>
+{
+public:
+    VKResourceAllocator() = default;
+    WIS_INLINE VKResourceAllocator(VKDeviceView device, VKAdapterView adapter)
     {
-    public:
-        VKResourceAllocator() = default;
-        WIS_INLINE VKResourceAllocator(VKDeviceView device, VKAdapterView adapter)
-        {
-            VmaAllocatorCreateInfo allocatorInfo{
-                VmaAllocatorCreateFlags(0),
-                std::get<0>(adapter),
-                device.get(),
-                0,
-                nullptr,
-                nullptr,
-                nullptr,
-                nullptr,
-                VKFactory::GetInstance(),
-                VKFactory::GetApiVer()
-            };
+        VmaAllocatorCreateInfo allocatorInfo{
+            VmaAllocatorCreateFlags(0),
+            std::get<0>(adapter),
+            device.get(),
+            0,
+            nullptr,
+            nullptr,
+            nullptr,
+            nullptr,
+            VKFactory::GetInstance(),
+            VKFactory::GetApiVer()
+        };
 
-            VmaAllocator al;
-            vmaCreateAllocator(&allocatorInfo, &al);
-            allocator = wis::shared_handle<VmaAllocator>{ al, std::move(device) };
-        }
+        VmaAllocator al;
+        vmaCreateAllocator(&allocatorInfo, &al);
+        allocator = wis::shared_handle<VmaAllocator> { al, std::move(device) };
+    }
 
-        /// @brief Create a buffer that is persistently mapped to the GPU
-        /// @param size Size of the buffer
-        /// @param flags Type of buffer
-        /// @return Buffer object
-        [[nodiscard]] VKBuffer CreatePersistentBuffer(size_t size, BufferFlags flags = BufferFlags::None) const
-        {
-            vk::BufferCreateInfo desc{
-                {}, size, vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits(flags), vk::SharingMode::eExclusive, 0, nullptr, nullptr
-            };
-            VmaAllocationCreateInfo alloc{
-                .usage = VmaMemoryUsage::VMA_MEMORY_USAGE_AUTO
-            };
-            return CreateBuffer(desc, alloc);
-        }
+    /// @brief Create a buffer that is persistently mapped to the GPU
+    /// @param size Size of the buffer
+    /// @param flags Type of buffer
+    /// @return Buffer object
+    [[nodiscard]] VKBuffer CreatePersistentBuffer(size_t size, BufferFlags flags = BufferFlags::None) const
+    {
+        vk::BufferCreateInfo desc{
+            {}, size, vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits(flags), vk::SharingMode::eExclusive, 0, nullptr, nullptr
+        };
+        VmaAllocationCreateInfo alloc{
+            .usage = VmaMemoryUsage::VMA_MEMORY_USAGE_AUTO
+        };
+        return CreateBuffer(desc, alloc);
+    }
 
-        /// @brief Create a buffer that is accessible by the CPU and serves as a staging buffer for GPU uploads
-        /// @param size Size of the buffer
-        /// @return Buffer object
-        [[nodiscard]] VKBuffer CreateUploadBuffer(size_t size) const
-        {
-            vk::BufferCreateInfo desc{
-                vk::BufferCreateFlags{}, size, vk::BufferUsageFlagBits::eTransferSrc,
-                vk::SharingMode::eExclusive, 0, nullptr, nullptr
-            };
-            VmaAllocationCreateInfo alloc{
-                .flags = VmaAllocationCreateFlagBits::VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT,
-                .usage = VmaMemoryUsage::VMA_MEMORY_USAGE_AUTO,
-                .requiredFlags = VkMemoryPropertyFlags(vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent) // ensure mapping does not need to be flushed
-            };
+    /// @brief Create a buffer that is accessible by the CPU and serves as a staging buffer for GPU uploads
+    /// @param size Size of the buffer
+    /// @return Buffer object
+    [[nodiscard]] VKBuffer CreateUploadBuffer(size_t size) const
+    {
+        vk::BufferCreateInfo desc{
+            vk::BufferCreateFlags{}, size, vk::BufferUsageFlagBits::eTransferSrc,
+            vk::SharingMode::eExclusive, 0, nullptr, nullptr
+        };
+        VmaAllocationCreateInfo alloc{
+            .flags = VmaAllocationCreateFlagBits::VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT,
+            .usage = VmaMemoryUsage::VMA_MEMORY_USAGE_AUTO,
+            .requiredFlags = VkMemoryPropertyFlags(vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent) // ensure mapping does not need to be flushed
+        };
 
-            return CreateBuffer(desc, alloc);
-        }
+        return CreateBuffer(desc, alloc);
+    }
 
-        /// @brief Create a buffer that is accessible by the CPU and GPU
-        /// @param size Size of the buffer
-        /// @param flags Type of buffer
-        /// @return Buffer object
-        [[nodiscard]] VKBuffer CreateHostVisibleBuffer(size_t size, BufferFlags flags = BufferFlags::None) const
-        {
-            vk::BufferCreateInfo desc{
-                vk::BufferCreateFlags{}, size, vk::BufferUsageFlagBits(flags),
-                vk::SharingMode::eExclusive, 0, nullptr, nullptr
-            };
-            VmaAllocationCreateInfo alloc{
-                .flags = VmaAllocationCreateFlagBits::VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT,
-                .usage = VmaMemoryUsage::VMA_MEMORY_USAGE_AUTO,
-                .requiredFlags = VkMemoryPropertyFlags(vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent) // ensure mapping does not need to be flushed
-            };
+    /// @brief Create a buffer that is accessible by the CPU and GPU
+    /// @param size Size of the buffer
+    /// @param flags Type of buffer
+    /// @return Buffer object
+    [[nodiscard]] VKBuffer CreateHostVisibleBuffer(size_t size, BufferFlags flags = BufferFlags::None) const
+    {
+        vk::BufferCreateInfo desc{
+            vk::BufferCreateFlags{}, size, vk::BufferUsageFlagBits(flags),
+            vk::SharingMode::eExclusive, 0, nullptr, nullptr
+        };
+        VmaAllocationCreateInfo alloc{
+            .flags = VmaAllocationCreateFlagBits::VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT,
+            .usage = VmaMemoryUsage::VMA_MEMORY_USAGE_AUTO,
+            .requiredFlags = VkMemoryPropertyFlags(vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent) // ensure mapping does not need to be flushed
+        };
 
-            return CreateBuffer(desc, alloc);
-        }
+        return CreateBuffer(desc, alloc);
+    }
 
-        /// @brief Create a constant buffer that is accessible by the CPU and GPU
-        /// This function is equivalent to CreateHostVisibleBuffer, but ensures that the buffer size is 256 byte aligned in debug mode
-        /// @param size Size of the buffer
-        /// @return Buffer object
-        [[nodiscard]] VKBuffer CreateConstantBuffer(size_t size) const
-        {
-            wis::assert_debug(size % 256 == 0, wis::format("{} is not 256 byte aligned", size));
-            return CreateHostVisibleBuffer(size, BufferFlags::ConstantBuffer);
-        }
+    /// @brief Create a constant buffer that is accessible by the CPU and GPU
+    /// This function is equivalent to CreateHostVisibleBuffer, but ensures that the buffer size is 256 byte aligned in debug mode
+    /// @param size Size of the buffer
+    /// @return Buffer object
+    [[nodiscard]] VKBuffer CreateConstantBuffer(size_t size) const
+    {
+        wis::assert_debug(size % 256 == 0, wis::format("{} is not 256 byte aligned", size));
+        return CreateHostVisibleBuffer(size, BufferFlags::ConstantBuffer);
+    }
 
-        [[nodiscard]] VKTexture CreateTexture(const TextureDescriptor& desc) const
-        {
-            auto format = convert_vk(desc.format);
-            vk::ImageCreateInfo img_desc{
-                vk::ImageCreateFlagBits::e2DArrayCompatible | vk::ImageCreateFlagBits::e2DViewCompatibleEXT,
-                vk::ImageType::e3D, format, vk::Extent3D{ desc.width, desc.height, desc.depth }, desc.mip_levels, desc.array_size
-            };
-            VmaAllocationCreateInfo alloc{
-                .usage = VmaMemoryUsage::VMA_MEMORY_USAGE_AUTO,
-            };
-            VkImage image;
-            VmaAllocation allocation;
-            VkResult a = vmaCreateImage(allocator.get(), reinterpret_cast<const VkImageCreateInfo*>(&img_desc), &alloc, &image, &allocation, nullptr);
-            return VKTexture{ format, wis::shared_handle<vk::Image>{ image, allocator.getParent() }, wis::shared_handle<VmaAllocation>{ allocation, allocator } };
-        }
+    [[nodiscard]] VKTexture CreateTexture(const TextureDescriptor& desc) const
+    {
+        auto format = convert_vk(desc.format);
+        vk::ImageCreateInfo img_desc{
+            vk::ImageCreateFlagBits::e2DArrayCompatible | vk::ImageCreateFlagBits::e2DViewCompatibleEXT,
+            vk::ImageType::e3D, format, vk::Extent3D{ desc.width, desc.height, desc.depth }, desc.mip_levels, desc.array_size
+        };
+        VmaAllocationCreateInfo alloc{
+            .usage = VmaMemoryUsage::VMA_MEMORY_USAGE_AUTO,
+        };
+        VkImage image;
+        VmaAllocation allocation;
+        VkResult a = vmaCreateImage(allocator.get(), reinterpret_cast<const VkImageCreateInfo*>(&img_desc), &alloc, &image, &allocation, nullptr);
+        return VKTexture{ format, wis::shared_handle<vk::Image>{ image, allocator.getParent() }, wis::shared_handle<VmaAllocation>{ allocation, allocator } };
+    }
 
-    private:
-        [[nodiscard]] VKBuffer CreateBuffer(const vk::BufferCreateInfo& desc, const VmaAllocationCreateInfo& alloc_desc) const
-        {
-            VmaAllocation allocation;
-            VkBuffer buffer;
-            VkResult a = vmaCreateBuffer(allocator.get(), reinterpret_cast<const VkBufferCreateInfo*>(&desc), &alloc_desc, &buffer, &allocation, nullptr);
-            return VKBuffer{ wis::shared_handle<vk::Buffer>{ buffer, allocator.getParent() }, wis::shared_handle<VmaAllocation>{ allocation, allocator }, desc.size };
-        }
-    };
+private:
+    [[nodiscard]] VKBuffer CreateBuffer(const vk::BufferCreateInfo& desc, const VmaAllocationCreateInfo& alloc_desc) const
+    {
+        VmaAllocation allocation;
+        VkBuffer buffer;
+        VkResult a = vmaCreateBuffer(allocator.get(), reinterpret_cast<const VkBufferCreateInfo*>(&desc), &alloc_desc, &buffer, &allocation, nullptr);
+        return VKBuffer{ wis::shared_handle<vk::Buffer>{ buffer, allocator.getParent() }, wis::shared_handle<VmaAllocation>{ allocation, allocator }, desc.size };
+    }
+};
 }
 
 #if defined(WISDOM_HEADER_ONLY)
