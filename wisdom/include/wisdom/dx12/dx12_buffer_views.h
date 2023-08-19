de0573708427195594a80bba53c336499b7ebabb
Restyled.io <commits@restyled.io>
2023-08-19 13:52:12 +0000

    Restyled by clang-format

--- wisdom/include/wisdom/dx12/dx12_buffer_views.h
+++ wisdom/include/wisdom/dx12/dx12_buffer_views.h
@@ -6,38 +6,38 @@
 
 WIS_EXPORT namespace wis
 {
-class DX12VertexBufferView;
+    class DX12VertexBufferView;
 
-template<>
-class Internal<DX12VertexBufferView>
-{
-public:
-    Internal() = default;
-    Internal(D3D12_VERTEX_BUFFER_VIEW view)
-        : view(view) { }
-
-    [[nodiscard]] D3D12_VERTEX_BUFFER_VIEW GetView() const noexcept
+    template<>
+    class Internal<DX12VertexBufferView>
     {
-        return view;
-    }
+    public:
+        Internal() = default;
+        Internal(D3D12_VERTEX_BUFFER_VIEW view)
+            : view(view) { }
 
-protected:
-    D3D12_VERTEX_BUFFER_VIEW view;
-};
+        [[nodiscard]] D3D12_VERTEX_BUFFER_VIEW GetView() const noexcept
+        {
+            return view;
+        }
 
-/// @brief Vertex buffer view
-class DX12VertexBufferView : public QueryInternal<DX12VertexBufferView>
-{
-public:
-    DX12VertexBufferView() = default;
-    explicit DX12VertexBufferView(D3D12_VERTEX_BUFFER_VIEW xhandle)
-        : QueryInternal(xhandle) { }
+    protected:
+        D3D12_VERTEX_BUFFER_VIEW view;
+    };
 
-    /// @brief Get the view
-    /// @note This function is only available in the internal API
-    operator D3D12_VERTEX_BUFFER_VIEW() const noexcept
+    /// @brief Vertex buffer view
+    class DX12VertexBufferView : public QueryInternal<DX12VertexBufferView>
     {
-        return view;
-    }
-};
+    public:
+        DX12VertexBufferView() = default;
+        explicit DX12VertexBufferView(D3D12_VERTEX_BUFFER_VIEW xhandle)
+            : QueryInternal(xhandle) { }
+
+        /// @brief Get the view
+        /// @note This function is only available in the internal API
+        operator D3D12_VERTEX_BUFFER_VIEW() const noexcept
+        {
+            return view;
+        }
+    };
 }
--- wisdom/include/wisdom/vulkan/vk_views.h
+++ wisdom/include/wisdom/vulkan/vk_views.h
@@ -6,25 +6,25 @@
 
 WIS_EXPORT namespace wis
 {
-using VKAdapterView = std::tuple<vk::PhysicalDevice>;
+    using VKAdapterView = std::tuple<vk::PhysicalDevice>;
 
-using VKDeviceView = wis::shared_handle<vk::Device>;
-using VKBufferView = vk::Buffer;
-struct VKTextureView {
-    vk::Image image;
-    vk::Format format;
-};
-using VKFenceView = vk::Semaphore;
-using VKCommandQueueView = vk::Queue;
-using VKCommandListView = vk::CommandBuffer;
-using VKPipelineStateView = vk::Pipeline;
-using VKRootSignatureView = vk::PipelineLayout;
-struct VKRenderPassView {
-    vk::RenderPass pass;
-    vk::Framebuffer frame;
-    Size2D frame_size;
-};
+    using VKDeviceView = wis::shared_handle<vk::Device>;
+    using VKBufferView = vk::Buffer;
+    struct VKTextureView {
+        vk::Image image;
+        vk::Format format;
+    };
+    using VKFenceView = vk::Semaphore;
+    using VKCommandQueueView = vk::Queue;
+    using VKCommandListView = vk::CommandBuffer;
+    using VKPipelineStateView = vk::Pipeline;
+    using VKRootSignatureView = vk::PipelineLayout;
+    struct VKRenderPassView {
+        vk::RenderPass pass;
+        vk::Framebuffer frame;
+        Size2D frame_size;
+    };
 
-using VKDescriptorSetView = vk::DescriptorSet;
-using VKDescriptorSetBindView = VKDescriptorSetView;
+    using VKDescriptorSetView = vk::DescriptorSet;
+    using VKDescriptorSetBindView = VKDescriptorSetView;
 }
