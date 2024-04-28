08415e01acb8b54bc53e03d8c67d9175cfb19899
Ilya Doroshenko <ilya.doroshenko@kdab.com>
2024-04-28 18:23:48 +0200

    p6:reverted VMA, will make header only later

--- wisdom/vulkan.cmake
+++ wisdom/vulkan.cmake
@@ -4,8 +4,6 @@ if(Vulkan_FOUND AND NOT WISDOM_WINDOWS_STORE)
 	message("Vulkan found!")
 	set(WISDOM_VULKAN TRUE)
 
-	find_package(vkma REQUIRED)
-
 	wisdom_sources(TARGET ${PROJECT_NAME}
 		HEADERS 
 			"include/wisdom/vulkan/vk_factory.h"
@@ -38,7 +36,7 @@ if(Vulkan_FOUND AND NOT WISDOM_WINDOWS_STORE)
 			WISDOM_VULKAN=1
 			WISVK_NO_DEFAULT_DELETER=1
 		LIBS
-			VulkanMemoryAllocator 
+			VKAllocator
 			wis::wisvk
 	)
 endif()
