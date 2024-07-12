78e988948be0cd3dbd6f04e4240a3ea38e180868
Ilya Doroshenko <ilya.doroshenko@kdab.com>
2024-07-13 00:14:50 +0200

    added more options
    added dxa dependency

--- wisdom/include/wisdom/config.h
+++ wisdom/include/wisdom/config.h
@@ -15,20 +15,7 @@
 #error "Platform not supported"
 #endif // _WIN32
 
-#if !defined(WISDOM_VULKAN) && __has_include(<vulkan/vulkan.h>)
-#define WISDOM_VULKAN 1
-#endif // WISDOM_VULKAN
-
-#if !defined(WISDOM_DX12) && defined(_WIN32) && __has_include(<d3d12.h>)
-#define WISDOM_DX12 1
-#endif // WISDOM_DX12
-
-#ifndef DEBUG_MODE && defined(_DEBUG)
-#define DEBUG_MODE 1
-#endif // DEBUG_MODE
-
 #define DEBUG_LAYER 0
-
 
 #endif // !WISDOM_UWP && !WISDOM_WINDOWS && !WISDOM_LINUX
 #endif // WISDOM_MANUAL_DETECT
