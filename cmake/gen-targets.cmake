18ec79c50e49025b44bd01e7108a685a42cbbc40
Ilya Doroshenko <ilya.doroshenko@kdab.com>
2024-07-12 20:35:58 +0200

    added more options to the package,
    Thanks, WinRT

--- cmake/gen-targets.cmake
+++ cmake/gen-targets.cmake
@@ -1,3 +1,5 @@
 message("Hello")
 
 configure_file(${CMAKE_CURRENT_LIST_DIR}/wisdom.targets ${CMAKE_INSTALL_PREFIX}/build/native/wisdom.targets @ONLY)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/wisdom.props ${CMAKE_INSTALL_PREFIX}/build/native/wisdom.props @ONLY)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/wisdom.xml ${CMAKE_INSTALL_PREFIX}/build/native/wisdom.xml @ONLY)
