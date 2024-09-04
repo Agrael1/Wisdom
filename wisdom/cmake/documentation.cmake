a256d3ec3bd2fe27b12991ca8f2b0641de8dec13
Restyled.io <commits@restyled.io>
2024-09-04 12:43:28 +0000

    Restyled by whitespace

--- wisdom/cmake/documentation.cmake
+++ wisdom/cmake/documentation.cmake
@@ -6,11 +6,11 @@ find_package(Doxygen)
         # set input and output files
         set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile.in)
         set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
-    
+
         # request to configure the file
         configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
         message("Doxygen build started")
-    
+
         # note the option ALL which allows to build the docs together with the application
         add_custom_target( doc_doxygen ALL
             COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
