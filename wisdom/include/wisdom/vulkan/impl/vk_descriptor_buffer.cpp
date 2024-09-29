ddd22c521faae38cfa1dac42ffc74912468a3c47
Restyled.io <commits@restyled.io>
2024-09-29 20:27:59 +0000

    Restyled by clang-format

--- wisdom/include/wisdom/vulkan/impl/vk_descriptor_buffer.cpp
+++ wisdom/include/wisdom/vulkan/impl/vk_descriptor_buffer.cpp
@@ -56,12 +56,12 @@ uint64_t wis::ImplVKDescriptorBuffer::WriteShaderResource(uint64_t buffer_offset
 }
 
 uint64_t wis::ImplVKDescriptorBuffer::WriteConstantBuffer(uint64_t buffer_offset_before_table,
-        uint32_t root_table_index,
-        uint32_t binding,
-        uint32_t array_member,
-        wis::VKRootSignatureView2 root_signature,
-        wis::VKBufferView buffer,
-        uint32_t buffer_size) noexcept
+                                                          uint32_t root_table_index,
+                                                          uint32_t binding,
+                                                          uint32_t array_member,
+                                                          wis::VKRootSignatureView2 root_signature,
+                                                          wis::VKBufferView buffer,
+                                                          uint32_t buffer_size) noexcept
 {
     auto& device = allocator.header();
     VkBufferDeviceAddressInfo address_info{
@@ -85,12 +85,12 @@ uint64_t wis::ImplVKDescriptorBuffer::WriteConstantBuffer(uint64_t buffer_offset
 }
 
 uint64_t wis::ImplVKDescriptorBuffer::WriteDescriptor(uint64_t buffer_offset_before_table,
-        uint32_t root_table_index,
-        uint32_t binding,
-        uint32_t array_member,
-        uint32_t descriptor_true_size,
-        const VkDescriptorGetInfoEXT& info,
-        wis::VKRootSignatureView2 root_signature) noexcept
+                                                      uint32_t root_table_index,
+                                                      uint32_t binding,
+                                                      uint32_t array_member,
+                                                      uint32_t descriptor_true_size,
+                                                      const VkDescriptorGetInfoEXT& info,
+                                                      wis::VKRootSignatureView2 root_signature) noexcept
 {
     auto& device = allocator.header();
     auto& dtable = device.table();
