e02cea0c9cf0d961207619de462d81d016c3616f
Ilya Doroshenko <ilya.doroshenko@kdab.com>
2024-10-06 18:41:54 +0200

    [Release] [CMake] fixed doc build

--- wisdom/cmake/documentation.cmake
+++ wisdom/cmake/documentation.cmake
@@ -1,7 +1,7 @@
 # check if Doxygen is installed
 find_package(Doxygen)
 
-#if(CMAKE_BUILD_TYPE MATCHES "^[Rr]elease")
+if(WISDOM_BUILD_DOCS)
     if (DOXYGEN_FOUND)
         # set input and output files
         set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile.in)
@@ -20,4 +20,4 @@ find_package(Doxygen)
     else()
         message("Doxygen need to be installed to generate the doxygen documentation")
     endif()
-#endif()
+endif()
