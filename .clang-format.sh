6429f9a1fefe9b9212d4c3e874bd5482dea53901
Restyled.io <commits@restyled.io>
2026-04-06 16:34:05 +0000

    Restyled by shellharden

--- .clang-format.sh
+++ .clang-format.sh
@@ -3,16 +3,16 @@
 FORMAT_TARGETS=("src" "examples")
 
 function format() {
-    for f in $(find $@ \( -name '*.h' -or -name '*.hpp' -or -name '*.ixx' -or -name '*.m' -or -name '*.mm' -or -name '*.c' -or -name '*.cpp' \) ! -name 'xxhash.h'); do 
-        echo "format ${f}";
-        clang-format -i "${f}";
+    for f in "$(find "$@" \( -name '*.h' -or -name '*.hpp' -or -name '*.ixx' -or -name '*.m' -or -name '*.mm' -or -name '*.c' -or -name '*.cpp' \) ! -name 'xxhash.h')"; do 
+        echo "format $f";
+        clang-format -i "$f";
     done
 
     echo "~~~ $@ Done ~~~";
 }
 
 for dir in "$FORMAT_TARGETS"; do
-    if [ -d "${dir}" ]; then
-        format "${dir}";
+    if [ -d "$dir" ]; then
+        format "$dir";
     fi
 done
