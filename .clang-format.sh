5691f21383a6c8dcb96f8b29828b1811f7f6967c
Restyled.io <commits@restyled.io>
2026-04-11 17:27:58 +0000

    Restyled by shfmt

--- .clang-format.sh
+++ .clang-format.sh
@@ -4,10 +4,10 @@ FORMAT_TARGETS=("src" "examples")
 
 function format() {
   find "$@" \( -name '*.h' -or -name '*.hpp' -or -name '*.ixx' -or -name '*.m' -or -name '*.mm' -or -name '*.c' -or -name '*.cpp' \) ! -name 'xxhash.h' -print0 |
-  while IFS= read -r -d '' f; do
-    echo "format $f"
-    clang-format -i "$f"
-  done
+    while IFS= read -r -d '' f; do
+      echo "format $f"
+      clang-format -i "$f"
+    done
 
   echo "~~~ $@ Done ~~~"
 }
