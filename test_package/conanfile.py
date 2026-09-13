0cfdddf1a5c71783dd5f78f916e5152c1982d5e3
Restyled.io <commits@restyled.io>
2026-09-13 17:10:05 +0000

    Restyled by reorder-python-imports

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,7 +2,9 @@ import os
 
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMake
+from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeToolchain
 
 
 class WisdomTestConan(ConanFile):
